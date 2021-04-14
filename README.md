c1c08edaf370fdbdaa950aecf9e71a08aec3f28b
liyatang <liyatang@qq.com>
2021-04-14 11:50:11 +0800

    feat: 一些优化

---
+++ README.md
@@ -0,0 +1,20 @@
+# MultiEnvWebpackPlugin
+
+根据环境解析不同的文件。 比如 src/index.js 解析 src/index.local.js
+
+# 使用
+
+坑
+
+# 原理
+
+TODO
+
+# 参考
+
+respect Taro 多端开发思想
+
+[Taro 多端开发](https://taro-docs.jd.com/taro/docs/envs#%E8%AE%A9-node_modules-%E4%B8%AD%E7%9A%84%E4%BE%9D%E8%B5%96%E4%B9%9F%E8%83%BD%E8%A7%A3%E6%9E%90%E5%A4%9A%E7%AB%AF%E6%96%87%E4%BB%B6)
+
+[MultiPlatformPlugin](https://github.com/NervJS/taro/blob/next/packages/taro-runner-utils/src/resolve/MultiPlatformPlugin.ts)
+
--- index.js
+++ index.js
@@ -1,86 +1,84 @@
-const path = require('path')
-const fs = require('fs')
+const path = require('path');
+const fs = require('fs');
 
-function resolveEnvFilePath(p, env, extArr = ['.js', '.jsx', '.ts', '.tsx']) {
-  const realPath = p
+// 核心场景是该 路由文件，所以优先 tsx jsx
+const defaultExtArr = ['.tsx', '.jsx', '.ts', '.js'];
 
+function resolveEnvFilePath (p, env, extArr = defaultExtArr) {
   for (let i = 0; i < extArr.length; i++) {
-    const item = extArr[i]
+    const item = extArr[i];
 
-    const pathArr = [`${p}.${env}${item}`, `${p}.${item}`]
-
-    for (let j = 0; j < pathArr.length; j++) {
-      const jItem = pathArr[j]
-      if (fs.existsSync(jItem)) {
-        return jItem
-      }
+    if (fs.existsSync(`${p}.${env}${item}`)) {
+      return `${p}.${env}${item}`;
     }
   }
-
-  return realPath
 }
 
 /**
- * MultiEnvWebpackPlugin 根据环境解析不同的文件。基于 enhance-resolve。
- * 比如 src/index.js 解析 src/index.local.js
+ * MultiEnvWebpackPlugin
  *
  * @param {string} [source='described-resolve'] source resolver hook 类别。
- * @param {string} [target='resolve'] 解析完成后需要触发的钩子
- * @param {object} options 插件配置项
+ * @param {string} [target='resolve'] 解析完成后需要触发的钩子。
+ * @param {Object} options 插件配置项。
+ * @param {string} options.env 环境，默认 local。
+ * @param {string[]} options.include 默认忽略 node_modules，如果需要提供 include ，比如 ['@ones-ai']。
  */
 class MultiEnvWebpackPlugin {
-  constructor(source = 'described-resolve', target = 'resolve', options) {
-    this.source = source
-    this.target = target
+  constructor (source = 'described-resolve', target = 'resolve', options) {
+    this.source = source;
+    this.target = target;
     this.options = Object.assign(
       {
         env: 'local',
       },
       options,
-    )
+    );
   }
 
-  apply(resolver) {
-    const target = resolver.ensureHook(this.target)
+  apply (resolver) {
+    const target = resolver.ensureHook(this.target);
     resolver
       .getHook(this.source)
       .tapAsync('MultiEnvWebpackPlugin', (request, resolveContext, callback) => {
-        const innerRequest = request.request || request.path
+        const innerRequest = request.request || request.path;
 
         // 使用 require.context 的时候 request.directory 为 true
         if (!innerRequest || request.directory) {
-          return callback()
+          return callback();
         }
 
-        // 有后缀
+        // 有后缀的忽略，算精准 import 了
         if (path.extname(innerRequest)) {
-          return callback()
+          return callback();
         }
 
-        let srcRequest
+        let srcRequest;
 
         if (path.isAbsolute(innerRequest)) {
           // absolute path
-          srcRequest = innerRequest
+          srcRequest = innerRequest;
         } else if (!path.isAbsolute(innerRequest) && /^\./.test(innerRequest)) {
           // relative path
-          srcRequest = path.resolve(request.path, request.request)
+          srcRequest = path.resolve(request.path, request.request);
         } else {
-          return callback()
+          return callback();
         }
 
         if (/node_modules/.test(srcRequest) && !this.includes(srcRequest)) {
-          return callback()
+          return callback();
         }
 
-        const newRequestStr = resolveEnvFilePath(srcRequest, this.options.env)
-        if (newRequestStr === innerRequest) {
-          return callback()
+
+        const newRequestStr = resolveEnvFilePath(srcRequest, this.options.env);
+        if (!newRequestStr) {
+          return callback();
         }
+
+        console.log('MultiEnvWebpackPlugin', srcRequest, newRequestStr)
 
         const obj = Object.assign({}, request, {
           request: newRequestStr,
-        })
+        });
 
         return resolver.doResolve(
           target,
@@ -89,29 +87,29 @@ class MultiEnvWebpackPlugin {
           resolveContext,
           (err, result) => {
             if (err) {
-              return callback(err)
+              return callback(err);
             }
 
             if (result === undefined) {
-              return callback(null, null)
+              return callback(null, null);
             }
 
-            return callback(null, result)
+            return callback(null, result);
           },
-        )
-      })
+        );
+      });
   }
 
-  includes(filePath) {
+  includes (filePath) {
     if (!this.options.include || !this.options.include.length) {
-      return false
+      return false;
     }
 
-    filePath = filePath.replace(path.sep, '/')
-    const res = this.options.include.find((item) => filePath.includes(item))
+    filePath = filePath.replace(path.sep, '/');
+    const res = this.options.include.find((item) => filePath.includes(item));
 
-    return Boolean(res)
+    return !!res;
   }
 }
 
-module.exports = MultiEnvWebpackPlugin
+module.exports = MultiEnvWebpackPlugin;
